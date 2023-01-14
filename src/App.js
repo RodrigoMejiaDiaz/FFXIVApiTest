40f27a50ca7c1093e164ef94e25b85fa7b4d6d7a
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2023-01-14 17:08:16 -0500

    Basic Form Validation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import FormValidation from './FormValidation';
 
-import InputEvents from './events/InputEvents'
-import MovementEvents from './events/MovementEvents'
-import './events/Events.css'
+// import InputEvents from './events/InputEvents'
+// import MovementEvents from './events/MovementEvents'
+// import './events/Events.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <InputEvents />
-        <MovementEvents />
+        <FormValidation />
+        {/* <InputEvents />
+        <MovementEvents /> */}
       </div>
     );
   }
