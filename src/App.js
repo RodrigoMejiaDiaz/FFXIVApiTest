06abfea8c76e28668a47c1d59109a044844069d6
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2023-01-14 13:49:27 -0500

    Usando State y Condicionales

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Card from './Card.js'
@@ -8,20 +7,8 @@ function App() {
   return (
     <div className="App">
       <Card />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card />
+      <Card />
     </div>
   );
 }
