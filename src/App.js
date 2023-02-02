96a9bdf9f7ffcb2d3948d99ec4ed0f07e7418cca
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2023-02-02 15:54:58 -0500

    Git Deploy test 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 // import Home from "./components/Home";
